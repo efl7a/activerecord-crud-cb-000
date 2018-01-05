eb58c8b0d81712050825fdc7d07c964ae8c350da
Heather Christman <efl7a@yahoo.de>
2018-01-05 21:08:52 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -89,5 +89,7 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.last(10).destroy
+  Movie.last(10).each do |m|
+    m.destroy
+  end
 end
